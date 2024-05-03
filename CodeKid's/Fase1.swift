37c416d74253a05f69f2e5666ac998965b59a4de
Francisco Arley Coelho Barros <contatofcarley@gmail.com>
2024-05-03 14:45:33 -0300

    Auterações na tela inicial e tamanho dos textos e botões (03/05/2024)

---
+++ CodeKid's/Fase1.swift
@@ -0,0 +1,18 @@
+//
+//  Fase1.swift
+//  CodeKid's
+//
+//  Created by found on 03/05/24.
+//
+
+import SwiftUI
+
+struct Fase1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Fase1()
+}
