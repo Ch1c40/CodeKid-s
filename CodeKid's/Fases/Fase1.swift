780369ab720d4a075b08cbbd0eaea8938cb4fbec
Francisco Arley Coelho Barros <contatofcarley@gmail.com>
2024-05-07 16:44:15 -0300

    Alterações do dia 7/05/24

--- CodeKid's/Fases/Fase1.swift
+++ CodeKid's/Fases/Fase1.swift
@@ -9,127 +9,57 @@ import SwiftUI
 
 
 struct Fase1: View {
+    
     var body: some View {
         ZStack{
             Image("Background1")
                 .blur(radius: 5.0)
                 .ignoresSafeArea()
-            VStack{
-                HStack{
-                    Rectangle()
-                        .frame(width: 100, height: 100)
-                        .foregroundStyle(.secondary)
-                        .border(Color.black)
-                    Rectangle()
-                        .frame(width: 100, height: 100)
-                        .foregroundStyle(.secondary)
-                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
-                    Rectangle()
-                        .frame(width: 100, height: 100)
-                        .foregroundStyle(.secondary)
-                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
-                    Rectangle()
-                        .frame(width: 100, height: 100)
-                        .foregroundStyle(.secondary)
-                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
-                    Rectangle()
-                        .frame(width: 100, height: 100)
-                        .foregroundStyle(.secondary)
-                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
-                }
-                HStack{
-                    Rectangle()
-                        .frame(width: 100, height: 100)
-                        .foregroundStyle(.secondary)
-                        .border(Color.black)
-                    Rectangle()
-                        .frame(width: 100, height: 100)
-                        .foregroundStyle(.secondary)
-                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
-                    Rectangle()
-                        .frame(width: 100, height: 100)
-                        .foregroundStyle(.secondary)
-                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
-                    Rectangle()
-                        .frame(width: 100, height: 100)
-                        .foregroundStyle(.secondary)
-                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
-                    Rectangle()
-                        .frame(width: 100, height: 100)
-                        .foregroundStyle(.secondary)
-                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
-                }
-                HStack{
-                    Rectangle()
-                        .frame(width: 100, height: 100)
-                        .foregroundStyle(.secondary)
-                        .border(Color.black)
-                    Rectangle()
-                        .frame(width: 100, height: 100)
-                        .foregroundStyle(.secondary)
-                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
-                    Rectangle()
-                        .frame(width: 100, height: 100)
-                        .foregroundStyle(.secondary)
-                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
-                    Rectangle()
-                        .frame(width: 100, height: 100)
-                        .foregroundStyle(.secondary)
-                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
-                    Rectangle()
-                        .frame(width: 100, height: 100)
-                        .foregroundStyle(.secondary)
-                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
-                }
-                HStack{
-                    Rectangle()
-                        .frame(width: 100, height: 100)
-                        .foregroundStyle(.secondary)
-                        .border(Color.black)
-                    Rectangle()
-                        .frame(width: 100, height: 100)
-                        .foregroundStyle(.secondary)
-                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
-                    Rectangle()
-                        .frame(width: 100, height: 100)
-                        .foregroundStyle(.secondary)
-                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
-                    Rectangle()
-                        .frame(width: 100, height: 100)
-                        .foregroundStyle(.secondary)
-                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
-                    Rectangle()
-                        .frame(width: 100, height: 100)
-                        .foregroundStyle(.secondary)
-                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
-                }
-                HStack{
-                    Rectangle()
-                        .frame(width: 100, height: 100)
-                        .foregroundStyle(.secondary)
-                        .border(Color.black)
-                    Rectangle()
-                        .frame(width: 100, height: 100)
-                        .foregroundStyle(.secondary)
-                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
-                    Rectangle()
-                        .frame(width: 100, height: 100)
-                        .foregroundStyle(.secondary)
-                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
-                    Rectangle()
-                        .frame(width: 100, height: 100)
-                        .foregroundStyle(.secondary)
-                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
-                    Rectangle()
-                        .frame(width: 100, height: 100)
-                        .foregroundStyle(.secondary)
-                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, maxHeight: .infinity)
+            
+            VStack {
+                LazyVGrid(columns: Array(repeating: GridItem(.fixed(100), spacing: 5), count: 5), spacing: 5) {
+                    ForEach(0..<25) { _ in
+                        Rectangle()
+                            .frame(width: 100, height: 100)
+                            .foregroundStyle(.secondary)
+                            .border(Color.black)
+                    }
                 }
             }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            
+            HStack {
+                LazyHGrid(rows: Array(repeating: GridItem(.fixed(75), spacing: 2), count: 5), content: {
+                    ForEach(0..<5){ _ in
+                        Rectangle()
+                            .frame(width: 75, height: 75)
+                            .foregroundStyle(.white.opacity(0.4))
+                            .border(Color.gray)
+                    }
+                })
+                .padding()
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            
+            VStack{
+                LazyVGrid(columns: Array(repeating: GridItem(.fixed(75), spacing: 1), count: 10), content: {
+                    ForEach(0..<10){ _ in
+                        Rectangle()
+                            .frame(width: 75, height: 75)
+                            .foregroundStyle(.white.opacity(0.4))
+                            .border(Color.gray)
+                    }
+                })
+                .padding([.bottom], 50)
+            }
+            .frame(maxHeight: .infinity, alignment: .bottom)
         }
     }
 }
 
 #Preview {
-    Fase1()
+    NavigationStack{
+        Fase1()
+    }
 }
--- CodeKid's/SelecaoFase.swift
+++ CodeKid's/SelecaoFase.swift
@@ -7,56 +7,72 @@
 
 import SwiftUI
 
+struct Stage: Identifiable {
+    let id: UUID
+    let name: String
+    
+    init(name: String) {
+        self.id = UUID()
+        self.name = name
+    }
+}
+
 struct SelecaoFase: View {
+    
+    let stages: [Stage] = [
+        Stage(name: "Fase 1"),
+        Stage(name: "Fase 2"),
+        Stage(name: "Fase 3"),
+        Stage(name: "Fase 4"),
+        Stage(name: "Fase 5")
+    ]
+    
+    func nextView(from stageName: String) -> some View {
+        switch stageName {
+        case "Fase 1":
+            return AnyView(Fase1())
+        case "Fase 2":
+            return AnyView(Fase2())
+        case "Fase 3":
+            return AnyView(Fase3())
+        case "Fase 4":
+            return AnyView(Fase4())
+        case "Fase 5":
+            return AnyView(Fase5())
+        default:
+            return AnyView(Homepage(nome: "Arley"))
+        }
+    }
+    
     var body: some View {
         ZStack{
             Image("Background1").ignoresSafeArea()
                 .blur(radius: 5.0)
-            HStack{
-                NavigationLink {
-                    Fase1()
-                } label: {
-                    Circle()
-                        .frame(width: 100)
-                        .foregroundStyle(.black)
-                        .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+            HStack {
+                ForEach(stages) { stage in
+                    NavigationLink {
+                        nextView(from: stage.name)
+                    } label: {
+                        VStack{
+                            Circle()
+                                .frame(width: 150)
+                                .foregroundStyle(.black)
+                                .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                                .padding()
+                            Text(stage.name)
+                                .font(.system(size: 50))
+                                .foregroundStyle(Color.black)
+                                .bold()
+                            HStack{
+                                ForEach(0..<3){ _ in
+                                    Image(systemName: "star")
+                                        .foregroundStyle(.black)
+                                        .font(.system(size: 30))
+                                }
+                            }
+                        }
                         .padding()
-                }
-                NavigationLink {
-                    Fase2()
-                } label: {
-                    Circle()
-                        .frame(width: 100)
-                        .foregroundStyle(.black)
-                        .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
-                        .padding()
-                }
-                NavigationLink {
-                    Fase3()
-                } label: {
-                    Circle()
-                        .frame(width: 100)
-                        .foregroundStyle(.black)
-                        .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
-                        .padding()
-                }
-                NavigationLink {
-                    Fase4()
-                } label: {
-                    Circle()
-                        .frame(width: 100)
-                        .foregroundStyle(.black)
-                        .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
-                        .padding()
-                }
-                NavigationLink {
-                    Fase5()
-                } label: {
-                    Circle()
-                        .frame(width: 100)
-                        .foregroundStyle(.black)
-                        .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
-                        .padding()
+                    }
                 }
             }
         }
@@ -64,5 +80,7 @@ struct SelecaoFase: View {
 }
 
 #Preview {
-    SelecaoFase()
+    NavigationStack {
+        SelecaoFase()
+    }
 }
